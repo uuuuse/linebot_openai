ca3ab49435119d83a679d44a74eadd89e1324145
uuuuse <41535166+uuuuse@users.noreply.github.com>
2023-12-07 16:31:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def handle_message(event):
                                 )
                             )
             elif isinstance(event, PostbackEvent):
-                if event.postback.data=='a':
+                if event.postback.data=="a":
                     line_bot_api.reply_message(
                                 event.reply_token,
                                 TemplateSendMessage(
