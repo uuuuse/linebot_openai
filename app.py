c9128fb63b15856aacd4a477b4197433a0da6569
uuuuse <41535166+uuuuse@users.noreply.github.com>
2023-12-08 01:25:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,6 @@ def handle_message(event):
 def handle_message(event):
     if isinstance(event, PostbackEvent):
         if event.postback.data == "chatbot":
-                if msg=="chatbot":
                     line_bot_api.reply_message(
                                 event.reply_token,
                                 TemplateSendMessage(
