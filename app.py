3bee578e293b0f4d1b80221e878ce571d4bb90cb
uuuuse <41535166+uuuuse@users.noreply.github.com>
2023-12-08 15:41:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ def handle_message(event):
         try:
             GPT_answer = GPT_response(msg,chatmodel=model)
         except:
-            GPT_answer = GPT_response(msg)
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer+model))
+            GPT_answer = GPT_response(msg)+model
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
     except:
         print(traceback.format_exc())
         line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
