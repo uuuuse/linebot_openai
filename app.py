2d8c05a266913ba04636ed7f17db377c0a238279
uuuuse <41535166+uuuuse@users.noreply.github.com>
2023-12-08 14:26:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def handle_message(event):
                                 )
                             )
     try:
-        GPT_answer = GPT_response(msg,model)
+        GPT_answer = GPT_response(msg,chatmodel=model)
         print(GPT_answer)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
     except:
