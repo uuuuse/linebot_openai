77e5c231021b5b08c7f42062597b35880223c795
uuuuse <41535166+uuuuse@users.noreply.github.com>
2023-12-09 20:50:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -220,10 +220,11 @@ def handle_message_Audio(event):
        for chuck in audio_content.iter_content():
            tf.write(chuck)      
     try:
-            Audio_answer=audioGPT_response(tf.name,audiomodel)
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(Audio_answer))
+        Audio_answer=audioGPT_response(tf.name,audiomodel)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(Audio_answer))
     except:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage('錯誤'))
+        print(traceback.format_exc())
+        line_bot_api.reply_message(event.reply_token, TextSendMessage('錯誤'))
     
 @handler.add(MemberJoinedEvent)
 def welcome(event):
