9333446da0c6f463d513047363666b0bde5efcc4
uuuuse <41535166+uuuuse@users.noreply.github.com>
2023-12-09 21:53:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def audioGPT_response(audio,audiomodel):
     response=client.audio.transcriptions.create(model=audiomodel,
                                                 file=audio,
                                                 response_format="text")
-    answer =response[0]
+    answer =response[0]['text']
     return answer
 def imageGPT_generate_response(imagetext,imagemodel):
     # 接收回應
